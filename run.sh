95766e213ee117850f375e734b7eb4b4493d1c49
Ying Wang <yingw787.personal@gmail.com>
2020-02-03 13:24:25 -0500

    Update run.sh.

--- run.sh
+++ run.sh
@@ -9,3 +9,25 @@
 
 DOCKER=$(which docker)
 GIT=$(which git)
+
+GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
+DOCKER_BASE_IMAGE='ubuntu:18.04'
+DOCKER_IMAGE_NAME='tinydevcrm-mvp:latest'
+DOCKER_CONTAINER_NAME='tinydevcrm-mvp'
+
+$DOCKER pull $DOCKER_BASE_IMAGE
+
+$DOCKER build $GIT_REPO_ROOT \
+    --tag $DOCKER_IMAGE_NAME
+
+CONTAINER_EXISTS=$($DOCKER ps -a --format '{{ .Names }}' --filter name=$DOCKER_CONTAINER_NAME)
+
+if [ -n "$CONTAINER_EXISTS" ];
+then
+    $DOCKER stop $DOCKER_CONTAINER_NAME && $DOCKER rm $DOCKER_CONTAINER_NAME
+fi
+
+$DOCKER run \
+    --name $DOCKER_CONTAINER_NAME \
+    --network=host \
+    -it $DOCKER_IMAGE_NAME

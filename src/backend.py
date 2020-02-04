8cff230526a6a08de0ec9c55c4c035c28e6ad823
Ying Wang <yingw787.personal@gmail.com>
2020-02-04 14:42:52 -0500

    Update backend.py.

--- src/backend.py
+++ src/backend.py
@@ -1,50 +1,52 @@
-#!/usr/bin/env python3
-"""Backend.
-"""
-
 import os
-
-from flask import request
-from flask import redirect
-from flask import Flask
-
+from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-
-UPLOAD_FILE = '/tmp'
 app.secret_key = "secret_key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FILE
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
-def hello_world():
-    print("Hit endpoint '/'.")
-    return "Hello, World!"
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload-file', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    print("Hit endpoint '/upload-file'.")
-
     import ipdb
     ipdb.set_trace()
 
     if request.method == 'POST':
+        # check if the post request has the file part
         if 'file' not in request.files:
-            print('No file part')
-            return redirect('/')
+            flash('No file part')
+            return redirect(request.url)
         file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
         if file.filename == '':
-            print('No file selected for uploading')
-            return redirect('/')
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
 
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FILE'], filename))
-        print('File successfully uploaded')
-        return redirect('/')
+    return "Hello World!"
+
+    # return '''
+    # <!doctype html>
+    # <title>Upload new File</title>
+    # <h1>Upload new File</h1>
+    # <form method=post enctype=multipart/form-data>
+    #   <input type=file name=file>
+    #   <input type=submit value=Upload>
+    # </form>
+    # '''
 
 if __name__=="__main__":
-    os.environ["FLASK_APP"] = "backend.py"
-    app.run(port=5000)
+    app.run()

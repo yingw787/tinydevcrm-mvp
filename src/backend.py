954a604e3ed36ca4e0444f1a5962003540fdd76f
Ying Wang <yingw787.personal@gmail.com>
2020-02-04 15:12:32 -0500

    Update src/backend.py clean up extraneous code.

--- src/backend.py
+++ src/backend.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/tmp'
@@ -9,44 +9,28 @@ app = Flask(__name__)
 app.secret_key = "secret_key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    import ipdb
-    ipdb.set_trace()
-
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            return "No file part!"
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
+            return "No selected file!"
+        if file and file.filename.endswith('.csv'):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print('saved file')
-            return redirect(request.url)
 
-    return "Hello World!"
+            persist_filename = os.path.join(
+                app.config['UPLOAD_FOLDER'],
+                filename
+            )
+            file.save(persist_filename)
+            return f"Saved file to '{persist_filename}' successfully!"
 
-    # return '''
-    # <!doctype html>
-    # <title>Upload new File</title>
-    # <h1>Upload new File</h1>
-    # <form method=post enctype=multipart/form-data>
-    #   <input type=file name=file>
-    #   <input type=submit value=Upload>
-    # </form>
-    # '''
+    return "Upload route with GET request!"
 
 if __name__=="__main__":
     app.run()

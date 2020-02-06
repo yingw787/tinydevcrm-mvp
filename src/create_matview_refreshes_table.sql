83047b028aacf9a557d6dc7a5ee011a53fbca71f
Ying Wang <yingw787.personal@gmail.com>
2020-02-06 14:41:19 -0500

    Update create_matview_refreshes_table.sql update comments.

--- src/create_matview_refreshes_table.sql
+++ src/create_matview_refreshes_table.sql
@@ -1,9 +1,13 @@
 -- Creates a table to store events where the job scheduler refreshes a certain
 -- materialized view.
 --
--- TODO: See whether there's a way in order to restrict the number of
+-- TODO: See whether there's a way in order to restrict the number of records
+-- stored, in order to avoid out of disk space errors because of constant event
+-- generation. I think shouldn't affect trigger definition or creation because
+-- you can make triggers that target update statements only, and deletion of
+-- records shouldn't be the same as updating. Need to explore this.
 
-CREATE TYPE matview_refresh_event_status AS ENUM ('new', 'sent')
+CREATE TYPE matview_refresh_event_status AS ENUM ('new', 'sent');
 
 DROP TABLE IF EXISTS matview_refresh_events;
 

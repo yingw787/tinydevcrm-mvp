1406401b44c47209a10dd9340d9c849876393183
Ying Wang <yingw787.personal@gmail.com>
2020-02-07 12:03:06 -0500

    Update entrypoint.sh.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,11 +3,9 @@
 # Script to run after Docker build process has completed and runtime
 # configuration begins.
 
-# Start the PostgreSQL database.
-#
-# Need to start with '&' because otherwise postgresql will shut down after bash
-# script completes (?). Cannot script into Dockerfile.
+# Start, then configure, PostgreSQL.
 service postgresql start &
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
 
 # Start the Flask backend server.
 python3 /app/backend.py &
